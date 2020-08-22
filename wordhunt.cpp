c1e75e45dcbd358687b678c8f5b50099c4dbe27d
Kevin Mao <kevinmao@college.harvard.edu>
2020-08-22 12:37:34 -0700

    Initializing setup

---
+++ wordhunt.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  cout << "Enter the words of the board, left to right, top to bottom: ";
+  char input[16];
+  cin >> input;
+  cout << input << endl;
+}
